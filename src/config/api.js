689e9be21c9c80c7e846d37128e996594e7477b9
AYYOUB BOUSKKAR <ayoubbouskkar@gmail.com>
2026-03-06 11:55:26 +0000

    fix api url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,3 @@
-export const getApiUrl = () => {
-    // Return backend API URL or standard json-server one. In a real app we'd use environment variables
-    return import.meta.env.VITE_API_URL || 'http://localhost:5000';
-};
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default API_URL;
